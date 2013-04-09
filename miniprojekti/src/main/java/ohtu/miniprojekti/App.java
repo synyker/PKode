4bc0793a981bc76763821726891444a7a5862f6e
Jonne Airaksinen <jonne.airaksinen@cs.helsinki.fi>
2013-04-09 13:02:16 +0300

    Added spring/maven project

---
+++ miniprojekti/src/main/java/ohtu/miniprojekti/App.java
@@ -0,0 +1,13 @@
+package ohtu.miniprojekti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

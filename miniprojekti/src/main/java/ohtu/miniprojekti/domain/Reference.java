bdd5f57bb10a46ca49fe9dcb0de084c9663187c6
Jonne Airaksinen <jonne.airaksinen@cs.helsinki.fi>
2013-04-09 22:38:53 +0300

    asd

--- miniprojekti/src/main/java/ohtu/miniprojekti/domain/Reference.java
+++ miniprojekti/src/main/java/ohtu/miniprojekti/domain/Reference.java
@@ -22,7 +22,7 @@ public class Reference {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
     @Column(name = "author")
     private String author;
     @Column(name = "title")
@@ -32,4 +32,26 @@ public class Reference {
     @Column(name="publishdate")
     private int year;
     
+    public Reference(String author, String title, String journal, int publishdate) {
+        this.author = author;
+        this.title = title;
+        this.journal = journal;
+        this.year = publishdate;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
 }

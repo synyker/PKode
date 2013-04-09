66934bd9e0bddff3d64d8046f72b439c1c01c2d3
Jonne Airaksinen <jonne.airaksinen@cs.helsinki.fi>
2013-04-09 15:47:36 +0300

    Lisaysformin teko, controllerin perustoiminta

---
+++ miniprojekti/src/main/java/ohtu/miniprojekti/ReferenceController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.miniprojekti;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ *
+ * @author jonne
+ */
+@Controller
+public class ReferenceController {
+    
+    @RequestMapping("add")
+    public void processAdd(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().write("Hello World!");
+        } catch (IOException ex) {
+            System.out.println("Ei onnistunut!");
+        }
+    }
+    @RequestMapping("index")
+    public void processIndex(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().write("index");
+        } catch (IOException ex) {
+            System.out.println("Ei onnistunut!");
+        }
+    }
+}
---
+++ miniprojekti/src/main/webapp/WEB-INF/add.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : add
+    Created on : Apr 9, 2013, 3:14:34 PM
+    Author     : jonne
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Viitehallintajärjestelmä</title>
+    </head>
+    <body>
+        <h1>Viitehallintajärjestelmä</h1>
+        <h2>Lisää viite</h2>
+        <form method="POST" action="/miniprojekti/add.jsp">
+            <p>Author:<input type="text" name="author"/></p>
+            <p>Title:<input type="text" name="title"/></p>
+            <p>Journal:<input type="text" name="journal"/></p>
+            <p>Year:<input type="text" name="year"/></p>
+            <input type="submit" value="Send" />
+            
+        </form>
+    </body>
+</html>

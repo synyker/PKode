b9b9c559075f0c43e1350bcf6a5cb74cbb3b4237
Krista Longi <krista.longi@helsinki.fi>
2013-04-16 00:35:24 +0300

    Referenceen lisätty tarvittavat uudet kentät, textid:n generointi ja vastaavat jUnit testit

--- miniprojekti/referenceDB-create.sql
+++ miniprojekti/referenceDB-create.sql
@@ -1,7 +1,10 @@
 create table Reference (
   id                        integer primary key AUTOINCREMENT,
+  textid                    varchar(255),
+  type                      varchar(255),
   author                    varchar(255),
   title                     varchar(255),
+  booktitle                 varchar(255),
   journal                   varchar(255),
   volume                    varchar(255),
   number                    varchar(255),
--- miniprojekti/src/test/java/ohtu.miniprojekti.domain/ReferenceTest.java
+++ miniprojekti/src/test/java/ohtu.miniprojekti.domain/ReferenceTest.java
@@ -1,7 +1,5 @@
 package ohtu.miniprojekti;
 
-
-
 import ohtu.miniprojekti.domain.Reference;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,48 +10,101 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author klongi
+ * @author krista, jonne, markus
  */
 public class ReferenceTest {
+
     Reference reference;
-    
+
     public ReferenceTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         reference = new Reference("Tekijä", "Otsikko", "Lehti", "", "", "2009", "", "", "");
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
     public void authorSetCorrectlyWhenCreatedandNameGiven() {
-        assertEquals(reference.getAuthor(), "Tekijä");
+        assertEquals("Tekijä", reference.getAuthor());
     }
-    
+
     @Test
     public void titleSetCorrectlyWhenCreatedandNameGiven() {
-        assertEquals(reference.getTitle(), "Otsikko");
+        assertEquals("Otsikko", reference.getTitle());
     }
-    
+
     @Test
     public void journalSetCorrectlyWhenCreatedandNameGiven() {
-        assertEquals(reference.getJournal(), "Lehti");
+        assertEquals("Lehti", reference.getJournal());
+    }
+
+    @Test
+    public void volumeSetCorrectlyWhenCreatedandEmptyStringGiven() {
+        assertEquals("", reference.getVolume());
+    }
+
+    @Test
+    public void typeCorrectWhenAddingArticle() {
+        assertEquals("article", reference.getType());
+    }
+
+    @Test
+    public void textIdGeneratedCorrectlyWhenOneAuthor() {
+        assertEquals("T09", reference.getTextid());
+    }
+
+    @Test
+    public void textIdGeneratedCorrectlyWhenMoreThanOneAuthor() {
+        reference = new Reference("Tekijä, Kalle; Sukunimi, Seppo; ääpeli, olli", "Otsikko", "Lehti", "", "", "2013", "", "", "");
+        assertEquals("TSä13", reference.getTextid());
+    }
+
+    @Test
+    public void textIdGeneratedCorrectlyWhenMoreThanOneAuthorAndWhiteSpaces() {
+        reference = new Reference("Tekijä, Kalle   ; Sukunimi, Seppo  ;  ääpeli, olli", "Otsikko", "Lehti", "", "", "2013", "", "", "");
+        assertEquals("TSä13", reference.getTextid());
+    }
+
+    @Test
+    public void textIdGeneratedCorrectlyWhenYearTooLong() {
+        reference = new Reference("Tekijä, Kalle; Sukunimi, Seppo; ääpeli, olli", "Otsikko", "Lehti", "", "", "20000000000013", "", "", "");
+        assertEquals("TSä13", reference.getTextid());
+    }
+
+    @Test
+    public void typeCorrectWhenAddingBook() {
+        reference = new Reference("Tekijä, Kalle; Sukunimi, Seppo; ääpeli, olli", "Otsikko", "2009", "", "");
+        assertEquals("book", reference.getType());
+    }
+
+    @Test
+    public void typeCorrectWhenAddingInproceedings() {
+        reference = new Reference("Tekijä, Kalle; Sukunimi, Seppo; ääpeli, olli", "Otsikko", "Lehti", "2009", "", "", "");
+        assertEquals("inproceedings", reference.getType());
     }
     
     @Test
-    public void volumeSetCorrectlyWhenCreatedandEmptyStringGiven() {
-        assertEquals(reference.getVolume(), "");
+    public void textidWorksIfNoAUthorGiven() {
+        reference = new Reference("", "Otsikko", "Lehti", "", "", "2013", "", "", "");
+        assertEquals("13", reference.getTextid());
+    }
+    
+     @Test
+    public void textidWorksIfNoYearGiven() {
+        reference = new Reference("Kalle", "Otsikko", "Lehti", "", "", "", "", "", "");
+        assertEquals("K", reference.getTextid());
     }
 }
